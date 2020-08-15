2bfc1a144e07caa6243735541ffbd4e8f492f302
swapneel <swap24gosavi@gmail.com>
2020-08-15 11:22:41 +0530

    Exception class

---
+++ src/main/java/com/swapgo/stocktrade/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.swapgo.stocktrade.exception;
+
+public class UserNotFoundException extends RuntimeException {
+	
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
