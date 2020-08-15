1803f865c3c0bb16195cb9ac0e98a1b4c76ff153
swapneel <swap24gosavi@gmail.com>
2020-08-15 11:21:44 +0530

    first working model

--- src/main/java/com/swapgo/stocktrade/controller/ShareController.java
+++ src/main/java/com/swapgo/stocktrade/controller/ShareController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,52 +20,59 @@ import com.swapgo.stocktrade.repository.ShareRepository;
 import com.swapgo.stocktrade.repository.UserRepository;
 
 @RestController
-public class UserController {
+public class ShareController {
 
 	@Autowired
-	private UserRepository userRepository;
+	ShareRepository shareRepository;
 	
 	@Autowired
-	private ShareRepository shareRepository;
+	private UserRepository userRepository;	
 	
-	@GetMapping("/users")
-	public List<User> getUsers(){
-		return userRepository.findAll();
-	}
 	
-	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	@GetMapping("/user/{id}/share")
+	public Share getUserShare(@PathVariable int id) {
 		Optional<User> user = userRepository.findById(id);
 		
 		if(!user.isPresent()) {
 			throw new UserNotFoundException("ID:"+ id);
 		}
-		
-		return user.get();
+		Optional<Share> share= shareRepository.findByUserId(id);
+		return share.get();
 	}
-
-	@DeleteMapping("/users/{id}")
-	public void deleteUser(@PathVariable int id) {
-		
-		Optional<Share> share = shareRepository.findByUserId(id);
-		
-		if(share.isPresent()) {
-			shareRepository.deleteById(share.get().getId());
-		}
-		userRepository.deleteById(id);
-	}
-
 	
-	@PostMapping("/adduser")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
-		User savedUser = userRepository.save(user);
+	@PostMapping("/user/{id}/addshare")
+	public ResponseEntity<Object> addShareToUser(@PathVariable int id,  @RequestBody Share share) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		if(!user.isPresent()) {
+			throw new UserNotFoundException("User does not exist:"+ share.getUser().getId());
+		}		
+		
+		share.setUser(user.get());
+		
+		Share savedShare = shareRepository.save(share);
 		
 		URI location = ServletUriComponentsBuilder
 			.fromCurrentRequest()
 			.path("/{id}")
-			.buildAndExpand(savedUser.getId()).toUri();
+			.buildAndExpand(savedShare.getId()).toUri();
 		
 		return ResponseEntity.created(location).build();
-		
 	}	
+	
+	/*
+	 * @DeleteMapping("/user/share/{scriptName}") public void
+	 * deleteShare(@PathVariable String scriptName) {
+	 * shareRepository.deleteByScriptName(scriptName); }
+	 */
+	
+	
+	@GetMapping("/user/share")
+	public List<Share> getAllShares(){
+		return shareRepository.findAll();
+	}
+	
+	
+	
 }
--- src/main/java/com/swapgo/stocktrade/repository/ShareRepository.java
+++ src/main/java/com/swapgo/stocktrade/repository/ShareRepository.java
@@ -1,11 +1,20 @@
 package com.swapgo.stocktrade.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.swapgo.stocktrade.model.Share;
 
 @Repository
 public interface ShareRepository extends JpaRepository<Share, Integer>{
-
+	Optional<Share> findByUserId(int id);
+	
+	/*
+	 * @Query("delete from Share s WHERE LOWER(s.scriptName) = LOWER(:scriptName)")
+	 * void deleteByScriptName(@Param("scriptName") String scriptName);
+	 */
 }
